d9443965ab9aeec26afb577c8f4f10687e3b9120
Makoto <FujiMakoto@users.noreply.github.com>
2020-02-02 22:56:47 -0500

    Application creation

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		SauceNao Application Class
+ * @author		<a href='https://github.com/FujiMakoto/ips-saucenao'>Makoto</a>
+ * @copyright	(c) 2020 Makoto
+ * @package		Invision Community
+ * @subpackage	SauceNao
+ * @since		03 Feb 2020
+ * @version		
+ */
+ 
+namespace IPS\saucenao;
+
+/**
+ * SauceNao Application Class
+ */
+class _Application extends \IPS\Application
+{
+	
+}
