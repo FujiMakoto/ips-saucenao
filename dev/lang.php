3b1c9ab33696f79410a182af82871809bd8f0595
Makoto <FujiMakoto@users.noreply.github.com>
2020-02-03 17:53:16 -0500

    Index language strings

--- dev/lang.php
+++ dev/lang.php
@@ -1,5 +1,41 @@
 <?php
 
 $lang = array(
-	'__app_saucenao'	=> "SauceNao"
+	'__app_saucenao'	=> "SauceNao",
+
+    /**
+     * SauceNao indexes
+     */
+    'snau_index_0'  => 'H-Magazines',
+    'snau_index_2'  => 'H-Game CG',
+    'snau_index_3'  => 'DoujinshiDB',
+    'snau_index_5'  => 'Pixiv',
+    'snau_index_6'  => 'Pixiv (Historical)',
+    'snau_index_8'  => 'Nico Nico Seiga',
+    'snau_index_9'  => 'Danbooru',
+    'snau_index_10' => 'drawr Images',
+    'snau_index_11' => 'Nijie Images',
+    'snau_index_12' => 'Yande.re',
+    'snau_index_15' => 'Shutterstock',
+    'snau_index_16' => 'FAKKU',
+    'snau_index_18' => 'H-Misc',
+    'snau_index_19' => '2D-Market',
+    'snau_index_20' => 'MediBang',
+    'snau_index_21' => 'Anime',
+    'snau_index_22' => 'H-Anime',
+    'snau_index_23' => 'Movies',
+    'snau_index_24' => 'Shows',
+    'snau_index_25' => 'Gelbooru',
+    'snau_index_26' => 'Konachan',
+    'snau_index_27' => 'Sankaku Channel',
+    'snau_index_28' => 'Anime-Pictures.net',
+    'snau_index_29' => 'e621.net',
+    'snau_index_30' => 'Idol Complex',
+    'snau_index_31' => 'bcy.net Illust',
+    'snau_index_32' => 'bcy.net Cosplay',
+    'snau_index_33' => 'PortalGraphics.net (Hist)',
+    'snau_index_34' => 'deviantArt',
+    'snau_index_35' => 'Pawoo.net',
+    'snau_index_36' => 'Madokami (Manga)',
+    'snau_index_37' => 'MangaDex',
 );
